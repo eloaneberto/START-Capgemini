4b03ec9b84b018c12daed889ce6c33f5539b1260
Eloane Berto <113455541+eloaneberto@users.noreply.github.com>
2023-04-21 23:49:56 -0300

    START Capgemini

---
+++ OperadoresAritmeticos/src/Aritmeticos.java
@@ -0,0 +1,33 @@
+
+public class Aritmeticos {
+
+	public static void main(String[] args) {
+		int num1, num2, num3;
+		
+		// soma
+		num1 = 10;
+		num2 = 20;
+		num3 = num1 + num2;
+		System.out.println("O resultado da soma é:" + num3);
+		
+		// subtracao
+		num1 = 10;
+		num2 = 20;
+		num3 = num1 - num2;
+		System.out.println("O resultado da subtracao é:" + num3);
+		
+		// multiplicacao
+		num1 = 10;
+		num2 = 20;
+		num3 = num1 * num2;
+		System.out.println("O resultado da multiplicacao é:" + num3);
+		
+		// divisao
+		num1 = 10;
+		num2 = 20;
+		num3 = num1 / num2;
+		System.out.println("O resultado da divisao é:" + num3);
+
+	}
+
+}
---
+++ TiposDeDados/src/TipoDado.java
@@ -0,0 +1,20 @@
+
+public class TipoDado {
+
+	public static void main(String[] args) {
+		// demosntração de alguns tipos de dados
+		int numero;
+		float decimalCurto;
+		double decimalLongo;
+		char caracter;
+		String cadeiaCaracteres;
+		
+		numero = 10;
+		decimalCurto = 10.5f;
+		decimalLongo = 20.5;
+		caracter = 'A';
+		cadeiaCaracteres = "Meu curso";
+
+	}
+
+}
